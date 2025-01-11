0bdffee427747a84205abc03928ad28dada281a9
Flame Anjal <kinganjalap@gmail.com>
2025-01-11 09:08:29 +0545

    Product design complete

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,17 @@
-const partnerSection = document.querySelector(".ourPartners");
+// let sections = document.querySelectorAll(".sectionnn");
+// let btn = document.querySelectorAll(".navBtn");
+// console.log(sections);
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       console.log("ok");
+//       btn[1].classList.add("activeNavigationButton");
+//     } else {
+//       //   entry.target.classList.remove("activeNavigationButton");
+//     }
+//   });
+// });
+// sections.forEach((section) => {
+//   observer.observe(section);
+// });
