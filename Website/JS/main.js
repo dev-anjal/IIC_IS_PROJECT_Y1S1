e72dbe421abd1db2f371504cda748e0d1ba86cec
Flame Anjal <kinganjalap@gmail.com>
2025-01-19 12:48:28 +0545

    Before 50% submission

---
+++ Website/JS/main.js
@@ -0,0 +1,34 @@
+/* global document */
+/*
+ * @param {function} loadingAnimation - creating a loading animation when website is loading
+
+*/
+
+// creating a loading animation when website is loading
+function loadingAnimation() {
+  setTimeout(() => {
+    console.log("loading function is called");
+    document.querySelector(".navigationBar").style.transform =
+      "translateY(0px)";
+  }, 130);
+
+  setTimeout(() => {
+    document.querySelector(".introFirstText").classList.add("rollTheIntro");
+  }, 260);
+  setTimeout(() => {
+    document.querySelector(".introSecondText").classList.add("slideIntroText");
+  }, 500);
+  setTimeout(() => {
+    document
+      .querySelector(".IntroWatch")
+      .style.setProperty("transform", "translateX(0%)");
+  }, 550);
+  setTimeout(() => {
+    document
+      .querySelector(".bottomIntroText")
+      .style.setProperty("opacity", "1");
+    document
+      .querySelector(".bottomIntroText")
+      .style.setProperty("transform", "translateY(0px)");
+  }, 1000);
+}
